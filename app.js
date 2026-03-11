a48848267675715225b44684205caee9ebace947
yaseen-8547 <ycs613678@gmail.com>
2026-03-11 20:53:31 +0530

    jwt auth learned,authmiddleware builded,jwt sign and verfication learned,post method for save notes finished

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ mongoose.connect(process.env.MONGO_URI)
 })
 .catch(()=>console.log("mongodb connection failure"))
 const UserSchema=new mongoose.Schema({UserName:{type:String,required:true},PassWord:{type:String,required:true}})
+const postSchema=new mongoose.Schema({title:{type:String,required:true},Content:{type:String,required:true},userId:{type:String,required:true}})
 const User =mongoose.model("User",UserSchema)
+const Post=mongoose.model("Post",postSchema)
+function authMiddleware(req,res,next){
+    const authHeader=req.headers.authorization
+    if(!authHeader){
+       return res.status(401).json({message:"no token"})
+    }
+    const token =authHeader.split(" ")[1]
+    const decoded =jwt.verify(token,"secretKey")
+    req.userId=decoded.userId
+    next()
+}
 app.post("/signup",async(req,res)=>{
     const {UserName,PassWord}=req.body
    try{
@@ -40,6 +52,18 @@ app.post("/signin",async(req,res)=>{
     catch(err){
         res.status(500).json({message:"signin failed"})
     }
+})
+app.post('/savenotes',authMiddleware,async(req,res)=>{
+    const {title,content}=req.body
+   try{
+     Post.create({title,content,userId:req.userId})
+     res.json({message:"saved successfully"})
+   }
+   catch(err){
+    res.status(500).json({message:"failed to save"})
+   }
+    
+     
 })
 const PORT=process.env.PORT||5000
 app.listen(PORT,()=>{
